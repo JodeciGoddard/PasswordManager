9b3a17f3648e58fb326858ea89ffb9ed344d2045
Jodeci <jodecig@hotmail.com>
2018-07-13 15:56:10 +1200

    Added Nav drawer

--- app/src/main/java/com/example/jodeci/passwordmanager/ProfileView.java
+++ app/src/main/java/com/example/jodeci/passwordmanager/ProfileView.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
@@ -31,6 +33,7 @@ public class ProfileView extends AppCompatActivity {
     private ListView view;
     private CustomAdapter adapter;
     private ArrayList<Entry> myList;
+    private DrawerLayout mDrawerLayout;
 
 
     @Override
@@ -53,6 +56,25 @@ public class ProfileView extends AppCompatActivity {
         });
         thread.run();
         initialiseListView();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
